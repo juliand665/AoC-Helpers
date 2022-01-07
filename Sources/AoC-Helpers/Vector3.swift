40868ea1f38469cf88ac1ea418de6e363eabf303
Julian Dunskus <julian.dunskus@gmail.com>
2022-01-07 04:40:45 +0100

    minor cleanup

--- Sources/AoC-Helpers/Vector3.swift
+++ Sources/AoC-Helpers/Vector3.swift
@@ -2,56 +2,60 @@ import SimpleParser
 import HandyOperators
 
 public struct Vector3: Hashable {
-	public static let zero = Vector3(x: 0, y: 0, z: 0)
+	public static let zero = Vector3(0, 0, 0)
 	
 	public var x, y, z: Int
+	
+	public init(x: Int, y: Int, z: Int) {
+		self.x = x
+		self.y = y
+		self.z = z
+	}
+	
+	public init(_ x: Int, _ y: Int, _ z: Int) {
+		self.init(x: x, y: y, z: z)
+	}
 	
 	public var absolute: Int {
 		abs(x) + abs(y) + abs(z)
 	}
 	
-	public static func + (lhs: Vector3, rhs: Vector3) -> Vector3 {
+	public static func + (lhs: Self, rhs: Self) -> Self {
 		lhs <- { $0 += rhs }
 	}
 	
-	public static func += (lhs: inout Vector3, rhs: Vector3) {
+	public static func += (lhs: inout Self, rhs: Self) {
 		lhs.x += rhs.x
 		lhs.y += rhs.y
 		lhs.z += rhs.z
 	}
 	
-	public static func - (lhs: Vector3, rhs: Vector3) -> Vector3 {
+	public static func - (lhs: Self, rhs: Self) -> Self {
 		lhs <- { $0 -= rhs }
 	}
 	
-	public static func -= (lhs: inout Vector3, rhs: Vector3) {
+	public static func -= (lhs: inout Self, rhs: Self) {
 		lhs.x -= rhs.x
 		lhs.y -= rhs.y
 		lhs.z -= rhs.z
 	}
 	
-	public static func * (vec: Vector3, scale: Int) -> Vector3 {
+	public static func * (vec: Self, scale: Int) -> Self {
 		vec <- { $0 *= scale }
 	}
 	
-	public static func * (scale: Int, vec: Vector3) -> Vector3 {
+	public static func * (scale: Int, vec: Self) -> Self {
 		vec <- { $0 *= scale }
 	}
 	
-	public static func *= (vec: inout Vector3, scale: Int) {
+	public static func *= (vec: inout Self, scale: Int) {
 		vec.x *= scale
 		vec.y *= scale
 		vec.z *= scale
 	}
 	
-	public func distance(to other: Vector3) -> Int {
+	public func distance(to other: Self) -> Int {
 		(self - other).absolute
-	}
-}
-
-extension Vector3 {
-	public init(_ x: Int, _ y: Int, _ z: Int) {
-		self.init(x: x, y: y, z: z)
 	}
 }
 
