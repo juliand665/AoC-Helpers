35214fef0e6d28f663e0e83655b7df78844adf40
Julian Dunskus <julian.dunskus@gmail.com>
2022-12-02 19:35:50 +0100

    add ascii helpers

--- Sources/AoC-Helpers/Input.swift
+++ Sources/AoC-Helpers/Input.swift
@@ -7,17 +7,32 @@ public func input(filename: String = "input") -> Substring {
 }
 
 extension StringProtocol {
+	@inlinable
 	public func lines() -> [SubSequence] {
 		split(separator: "\n", omittingEmptySubsequences: false)
 	}
 	
+	@inlinable
 	public func words() -> [SubSequence] {
 		split(separator: " ")
 	}
 }
 
 extension Sequence where Element: StringProtocol {
+	@inlinable
 	public func asInts() -> [Int] {
 		map { Int($0)! }
+	}
+}
+
+extension Character {
+	@inlinable
+	public static func - (query: Self, base: Self) -> Int {
+		Int(query.asciiValue! - base.asciiValue!)
+	}
+	
+	@inlinable
+	public static func + (base: Self, offset: Int) -> Self {
+		.init(.init(base.asciiValue! + .init(offset)))
 	}
 }
