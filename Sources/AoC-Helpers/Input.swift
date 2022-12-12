8e5a3e4c4cb0839110ace52d5badf783295f1c8b
Julian Dunskus <julian.dunskus@gmail.com>
2022-12-12 02:02:54 +0100

    lots of parsing overloads

--- Sources/AoC-Helpers/Input.swift
+++ Sources/AoC-Helpers/Input.swift
@@ -14,6 +14,19 @@ extension StringProtocol {
 	}
 	
 	@inlinable
+	public func lineGroups() -> [ArraySlice<SubSequence>] {
+		split(separator: "\n", omittingEmptySubsequences: false)
+			.split(whereSeparator: \.isEmpty)
+	}
+	
+	@_disfavoredOverload
+	@available(macOS 13.0, *)
+	@inlinable
+	public func lineGroups() -> [SubSequence] {
+		split(separator: "\n\n")
+	}
+	
+	@inlinable
 	public func words() -> [SubSequence] {
 		split(separator: " ")
 	}
@@ -51,6 +64,62 @@ extension Parser {
 			if $0.isDone { return nil }
 			return $0.readInt()
 		})
+	}
+	
+	@inlinable
+	public func int(allowSigns: Bool = false) -> Int {
+		ints(allowSigns: allowSigns).onlyElement()!
+	}
+	
+	@inlinable
+	@discardableResult
+	public mutating func consumeLine() -> Substring {
+		consume(through: "\n") ?? consumeRest()
+	}
+	
+	@inlinable
+	public mutating func line() -> Self {
+		.init(reading: consumeLine())
+	}
+	
+	public func auto() -> String {
+		.init(input)
+	}
+	
+	public func auto() -> [Int] {
+		ints()
+	}
+	
+	public func auto() -> Int {
+		ints().onlyElement()!
+	}
+	
+	public func auto() -> (Int, Int) {
+		ints().extract()
+	}
+	
+	public func auto() -> (Int, Int, Int) {
+		ints().extract()
+	}
+	
+	public func auto() -> (Int, Int, Int, Int) {
+		ints().extract()
+	}
+	
+	public func auto() -> (Int, Int, Int, Int, Int) {
+		ints().extract()
+	}
+	
+	public func auto() -> (Int, Int, Int, Int, Int, Int) {
+		ints().extract()
+	}
+	
+	public func auto() -> (Int, Int, Int, Int, Int, Int, Int) {
+		ints().extract()
+	}
+	
+	public func auto() -> (Int, Int, Int, Int, Int, Int, Int, Int) {
+		ints().extract()
 	}
 }
 
