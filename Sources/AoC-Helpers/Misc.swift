ba8998b80fc8848cf9bd6c5bd3d3ed0daa02d063
Julian Dunskus <julian.dunskus@gmail.com>
2022-12-24 19:32:17 +0100

    auto-incremented string id replacements

--- Sources/AoC-Helpers/Misc.swift
+++ Sources/AoC-Helpers/Misc.swift
@@ -20,3 +20,22 @@ public extension BinaryInteger {
 		return simple >= 0 ? simple : simple + count
 	}
 }
+
+/// stores the auto-incremented IDs for `getID`
+public private(set) var rawIDs: [String] = []
+/// provides auto-incremented IDs
+@inlinable
+public func getID(_ raw: String) -> Int {
+	if let index = rawIDs.firstIndex(of: raw) {
+		return index
+	} else {
+		rawIDs.append(raw)
+		return rawIDs.count - 1
+	}
+}
+
+/// provides auto-incremented IDs
+@inlinable
+public func getID(_ raw: some StringProtocol) -> Int {
+	getID(String(raw))
+}
